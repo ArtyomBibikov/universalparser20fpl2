541c5e6011e188b8d2feb584a4bf03d5b0a843d8
Dana <33372286+Danessia@users.noreply.github.com>
2022-10-20 16:18:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-
+import validators
 
 class Scrapper:
     """
@@ -24,7 +24,7 @@ class Scrapper:
         options = Options()
         options.headless = True
         options.add_argument("--window-size=1920,1200")
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(options=options)
         if self.driver:
             status = True
         return status
@@ -35,26 +35,34 @@ class Scrapper:
         """
         status = False
         val = input("Enter a url: ")
-        wait = WebDriverWait(self.driver, 10)
-        self.driver.get(val)
-        wait.until(EC.url_to_be(val))
-        if self.driver.current_url == val:
-            self.page_source = self.driver.page_source
-            status = True
+        if validators.url(val):
+            wait = WebDriverWait(self.driver, 10)
+            self.driver.get(val)
+            wait.until(EC.url_to_be(val))
+            if self.driver.current_url == val:
+                self.page_source = self.driver.page_source
+                status = True
         return status
 
-    def scrollAndGetText(self):
+    def set_url(self, val):
+        """
+        set url
+        """
+        status = False
+        if validators.url(val):
+            wait = WebDriverWait(self.driver, 10)
+            self.driver.get(val)
+            wait.until(EC.url_to_be(val))
+            if self.driver.current_url == val:
+                self.page_source = self.driver.page_source
+                status = True
+
+        return status
+
+    def getText(self):
         """
         Get url text
         """
-        page_height = self.driver.execute_script("return document.documentElement.scrollHeight")
-        while True:
-            self.driver.execute_script("window.scrollTo(0,document.documentElement.scrollHeight);")
-            new_height = self.driver.execute_script("return document.documentElement.scrollHeight")
-            if new_height == page_height:
-                break
-            page_height = new_height
-
         status = False
         soup = BeautifulSoup(self.page_source, "html.parser")
         for element in soup(["script", "style"]):  # убираем скрипты и стиль
@@ -74,12 +82,11 @@ class Scrapper:
         fl.write(self.text)
         fl.close()
 
-
 if __name__ == "__main__":
     scrapper = Scrapper()
     if scrapper.createDriver():
         if scrapper.input_url():
-            if scrapper.scrollAndGetText():
+            if scrapper.getText():
                 scrapper.saveTextToFile('URL-text.txt')
             else:
                 print('Error while getting url text')
